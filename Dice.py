38a919f2e68082120f692aa90bf9a95cc4ca6229
Krishna <kt284007@gmail.com>
2025-05-13 23:01:23 +0545

    bonus round

--- Dice.py
+++ Dice.py
@@ -3,7 +3,6 @@ import random
 def roll_dice():
     """
     Simulates rolling a six-sided die.
-    
     Returns:
         int: A random integer between 1 and 6, inclusive.
     """
@@ -17,10 +16,23 @@ def main():
         result= roll_dice()
         print(f"you rolled a {result}!")
 
-        choice=input("Do you want to roll again? (y/n): ").strip().lower()
-        if choice !="y":
-            print("Thanks for playing!")
-            break
+        """choice=input("Do you want to roll again? (y/n): ").strip().lower()
+        if choice !="y":"""
+        if result== 6 or result==1:
+            print("you have one more bonus chance to roll")
+            bonus_result=roll_dice()
+            print(f"you rolled a {bonus_result}!")
+            if bonus_result==6 or bonus_result==1:
+                print("you have one more bonus chance to roll")
+                bonus_result=roll_dice()
+                print(f"you rolled a {bonus_result}!")
+        else:
+            choice=input("Do you want to roll again? (y/n): ").strip().lower()
+            if choice != "y":
+    
+                print("Thanks for playing!")
+                break
+        
 
 if __name__=="__main__":
     main()
